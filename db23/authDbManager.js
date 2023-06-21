9e2c7c1bb29ef6b37d14538a9b57a4827d30a939
gopal-sakshi <gopal612@gmail.com>
2023-06-21 21:23:44 +0530

    syntax explained

--- db23/authDbManager.js
+++ db23/authDbManager.js
@@ -21,3 +21,23 @@ authDb.org23 = require("./auth23/org23")(authDbInstance, Sequelize);
 authDb.user23 = require("./auth23/user23")(authDbInstance, Sequelize);
 
 module.exports = authDb;
+
+/********************************************************
+
+SYNTAX explained
+
+// This function 'model23' takes two params ---> databaseInstance & SequelizeLibrary
+    // it returns a "model"
+    // we create a property on authDb
+    // and assign this property that returned "model"
+
+const model23 = (seq, Dt) => {
+    const org23 = sequelize.define('org23', { }, { tableName: '', timeStamps: false });
+    return org23;
+}
+
+authDb.org23 = model23(authDbInstance, Sequelize);
+
+
+
+********************************************************/
